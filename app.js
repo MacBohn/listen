f0334b92d6dd465258506d2e1cac6e21159e52bf
McCartney Bohn <mccartneybohn@yahoo.com>
2015-01-16 17:27:16 -0700

    goo push state

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
 "use strict";
 $( document ).ready(function() {
-    
-  $('.container').on('click', '.fa-play', function() {
-    $('.fa').removeClass('fa-pause').addClass('fa-play');
-    $(this).removeClass('fa-play').addClass('fa-pause');
+
+
+
+$('.container').on('click', '.fa-play', function() {
+  $('.fa').removeClass('fa-pause').addClass('fa-play');
+  $(this).removeClass('fa-play').addClass('fa-pause');
+});
+
+$('.container').on('click', '.fa-pause', function() {
+  $(this).removeClass('fa-pause').addClass('fa-play');
+});
+
+
+
+  jQuery.getJSON('data.json', function(tracks) {
+
+    var $tracksTemplate = $('#tracksTemplate').html();
+
+    var newHTML = Mustache.to_html($tracksTemplate, tracks);
+
+    $('.player').html(newHTML);
   });
 
-  $('.container').on('click', '.fa-pause', function() {
-    $(this).removeClass('fa-pause').addClass('fa-play');
-  });
 
-})
+
+});
