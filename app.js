48dc26fb9be29d546e0920458a999f70c0d16861
McCartney Bohn <mccartneybohn@yahoo.com>
2015-01-20 14:27:49 -0700

    music player

--- app.js
+++ app.js
@@ -2,27 +2,31 @@
 $( document ).ready(function() {
 
 
+  $('.container').on('click', '.fa-play', function() {
+    var id = $(this).data('id')
+    document.getElementById(id).play()
+    $('.fa').removeClass('fa-stop').addClass('fa-play');
+    $(this).removeClass('fa-play').addClass('fa-stop');
+    var title = $(this).data('title')
+    $('.header').html("Now Playing: " + title )
+  });
 
-$('.container').on('click', '.fa-play', function() {
-  $('.fa').removeClass('fa-pause').addClass('fa-play');
-  $(this).removeClass('fa-play').addClass('fa-pause');
-});
+  $('.container').on('click', '.fa-stop', function() {
+    var id = $(this).data('id')
+    document.getElementById(id).stop()
+    $(this).removeClass('fa-stop').addClass('fa-play');
+    $('.header').html("Select a song!")
+  });
 
-$('.container').on('click', '.fa-pause', function() {
-  $(this).removeClass('fa-pause').addClass('fa-play');
-});
-
-
-
-  jQuery.getJSON('data.json', function(tracks) {
-
+  $.getJSON('data.json', function(tracks) {
     var $tracksTemplate = $('#tracksTemplate').html();
-
     var newHTML = Mustache.to_html($tracksTemplate, tracks);
-
     $('.player').html(newHTML);
   });
 
-
-
+  $.getJSON('data.json', function(id) {
+    var $headerTemplate = $('#headerTemplate').html();
+    var newHTML = Mustache.to_html($headerTemplate, id);
+    $('.header').html(newHTML);
+  });
 });
